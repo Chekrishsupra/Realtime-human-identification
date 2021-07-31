dfabbc35e9e609e694030e19a83f20df76eedd08
Chekrishsuper <78082844+Chekrishsupra@users.noreply.github.com>
2021-07-31 20:47:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-
 function setup() {
     canvas= createCanvas(200,200);
-    canvas.center();
+    video = createCapture(VIDEO);
+    canvas.center()
+    video.hide();
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/5ncqFi2zE/model.json",ModelLoaded);
     }
     function draw() {
-    
+     image(video,0,0,200,200);
+     classifier.classify(video, gotResult);
+    }
+    function ModelLoaded() {
+        console.log("Model Loaded");
+    }
+    function gotResult(error, results) {
+     if(error) {
+         console.error(error);
+     }
+     else {
+         console.log(results);
+         document.getElementById("result_object_name").innerHTML = results[0].label;
+         document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+     }
     }
